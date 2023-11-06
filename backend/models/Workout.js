89f64d09f37a6ebcd2d165f537b8dfc06021598e
celuu <christine.e.luu@gmail.com>
2023-11-06 12:16:26 -0800

    Remove extra id

--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -3,11 +3,6 @@ const Schema = mongoose.Schema
 
 const workoutSchema = new Schema(
   {
-    id: {
-      type: mongoose.Schema.Types.ObjectId,
-      default: () => new mongoose.Types.ObjectId(),
-      alias: "_id",
-    },
     user_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
