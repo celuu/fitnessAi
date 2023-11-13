f8bed013905259317f5ceba48a970a4248032ec5
celuu <christine.e.luu@gmail.com>
2023-11-12 21:03:34 -0800

    Update font

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,9 +1,10 @@
 import { Box } from "@chakra-ui/react";
 import WithSubnavigation from "./NavigationBar";
 const home = () => {
-    <> 
+    <Box color={"blue.100"}> 
+      hello
       <WithSubnavigation />
-    </>;
+    </Box>;
 
 
 }
